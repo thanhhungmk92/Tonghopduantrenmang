d0ebb75bfcbe1a397ca188b852852e92475cb36b
thanhhungmk92 <thanhhungmk92@gmail.com>
2018-11-17 11:00:33 +0700

    1

---
+++ ttcscn/sourcecode/ttcs/app/Http/ViewComposers/SidebarComposer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\ViewComposers;
+
+use Illuminate\View\View;
+use App\Tag;
+use App\Post;
+
+class SidebarComposer
+{
+    /**
+     * The user repository implementation.
+     *
+     * @var  UserRepository
+     */
+
+    /**
+     * Create a new profile composer.
+     *
+     * @param    UserRepository  $users
+     * @return  void
+     */
+    public function __construct()
+    {
+        // Dependencies automatically resolved by service container...
+        //$this->users = $users;
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param    View  $view
+     * @return  void
+     */
+    public function compose(View $view)
+    {
+        $tags = Tag::limit(10)->get();
+        $posts = Post::where('status',1)->orderBy('created_at','des')->get();
+        $video = Post::where('post_type','video')->orderBy('created_at','des')->take(1)->get();
+        $view->with('posts',$posts)->with('tags',$tags)->with('video', $video);
+       
+    }
+}
